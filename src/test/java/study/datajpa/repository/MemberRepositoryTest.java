df6a544e02fe970c3dddeb2402d9a35fa3181b55
thguss <thgus345@naver.com>
2023-03-08 22:15:57 +0900

    Section4: @Query, 값, DTO 조회하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -14,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +24,8 @@ class MemberRepositoryTest {
 
 	@Autowired
 	MemberRepository memberRepository;
+	@Autowired
+	TeamRepository teamRepository;
 
 	@Test
 	public void testMember() {
@@ -102,5 +106,33 @@ class MemberRepositoryTest {
 			assertThat(member.getAge()).isEqualTo(10);
 		}
 		assertThat(result.size()).isEqualTo(1);
+	}
+
+	@Test
+	public void findUsernameList() {
+		Member member1 = new Member("AAA", 10);
+		Member member2 = new Member("BBB", 20);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		List<String> usernameList = memberRepository.findUsernameList();
+		for (String s : usernameList) {
+			System.out.println("s = " + s);
+		}
+	}
+
+	@Test
+	public void findMemberDto() {
+		Team team = new Team("teamA");
+		teamRepository.save(team);
+
+		Member member = new Member("AAA", 10);
+		member.setTeam(team);
+		memberRepository.save(member);
+
+		List<MemberDto> memberDtoList = memberRepository.findMemberDto();
+		for (MemberDto memberDto : memberDtoList) {
+			System.out.println("dto = " + memberDto);
+		}
 	}
 }
