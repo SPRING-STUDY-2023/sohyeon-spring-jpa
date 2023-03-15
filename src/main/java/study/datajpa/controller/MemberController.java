fee246b9456aa61766cc96efdd232026c6378618
thguss <thgus345@naver.com>
2023-03-15 23:07:21 +0900

    Section5: Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -3,11 +3,15 @@ package study.datajpa.controller;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -25,12 +29,20 @@ public class MemberController {
 	}
 
 	@GetMapping("/members2/{id}")
-	public String findMember2(@PathVariable("id") Member member) {
+	public String findMember2(@PageableDefault(size = 5) @PathVariable("id") Member member) {
 		return member.getUsername( );
+	}
+
+	@GetMapping("/members")
+	public Page<MemberDto> list(Pageable pageable) {
+		return memberRepository.findAll(pageable)
+			.map(MemberDto::new);
 	}
 
 	@PostConstruct
 	public void init() {
-		memberRepository.save(new Member("memberA"));
+		for (int i = 0; i < 100; i++) {
+			memberRepository.save(new Member("user" + i, i));
+		}
 	}
 }
